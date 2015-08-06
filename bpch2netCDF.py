f1c1662e446762627f6f3ab5882e40faae8c0fea
Ben Newsome <bn506@york.ac.uk>
2015-08-06 14:56:02 +0100

    Updated ability / fixed ability to be ran from in python

--- bpch2netCDF.py
+++ bpch2netCDF.py
@@ -8,7 +8,7 @@
 
 def convert_to_netCDF(folder='none',filename='ctm.nc'):
 
-   
+   from bpch2netCDF import get_folder
    folder = get_folder(folder)
 
    bpch_files = folder + '/*.ctm.bpch'
@@ -26,27 +26,28 @@ def convert_to_netCDF(folder='none',filename='ctm.nc'):
    import iris
    iris.fileformats.netcdf.save(data, output_file)
 
-   def get_folder(folder):
-   
-      import sys
-      if folder=='none':
-         # getting the folder location from system argument
-         if len(sys.argv)<=1:
-            print "No folder location specified for the data"
-            sys.exit()
-         else:
-            folder = str(sys.argv[1])
-   
-      # Check folder exists
-      import os
-      if not os.path.exists( folder ):
-         print "Folder does not exist"
-         print folder
-         sys.exit()
-   
-   
-      return folder;
    return
+
+def get_folder(folder):
+
+   import sys
+   if folder=='none':
+      # getting the folder location from system argument
+      if len(sys.argv)<=1:
+         print "No folder location specified for the data"
+         sys.exit()
+      else:
+         folder = str(sys.argv[1])
+
+   # Check folder exists
+   import os
+   if not os.path.exists( folder ):
+      print "Folder does not exist"
+      print folder
+      sys.exit()
+
+
+   return folder;
    
 if __name__ == "__main__":
    convert_to_netCDF()
