32d66863aaedc5f8ecd2a4e84bd3893457c80500
ts551 <ts551@york.ac.uk>
2016-01-25 11:04:14 +0000

    update animate array + pull AC_tools head

--- animate_array.py
+++ animate_array.py
@@ -6,11 +6,13 @@ import gc
 import datetime
 
 debug=True
-specs = [ 'O3', 'NO2' ]
+specs = [ 'O3', 'NO2' , 'PAN', 'ALD2' ]
+pcent=True
+ClearFlo_unit=True
 
 def main( spec='NO' , pcent=False, fixcb=None, limit_by_dates=False, \
-        extend='neither', debug=False ):
-    """ Extract data array from given location and make Animation"""
+        extend='neither', ClearFlo_unit=False, verbose=True, debug=False ):
+    """ Extract data array from given location and make Animation """
 
     # Get data in array ( time, lat, lon ) and dates (datetime.datetime )
     arr, dates = get_data_dates( spec=spec, limit_by_dates=limit_by_dates,
@@ -18,62 +20,59 @@ def main( spec='NO' , pcent=False, fixcb=None, limit_by_dates=False, \
     if debug:
         print [ (i[:5], i.shape ) for i in arr, dates]
     
-    # Get titles and other varibles for run (e.g. res )
-    lat, lon, units, fname, res, title, scale = get_run_info( spec=spec )
+    # Get titles and other varibles for run (e.g. res, lons and lats for GC config. )
+    lat, lon, units, fname, res, title, scale = get_run_info( spec=spec, \
+            pcent=pcent, ClearFlo_unit=ClearFlo_unit )
     arr = arr*scale
 
     # Setup figure and axis
     fig, ax = setup_figure_and_axis( )
 
-    # manual limit colormap ( colorbar )
+    # Set manual limit colormap ( colorbar )? 
 #    fixcb = np.array([ 0, 100])
 #    extend ='both'
+#    extend ='max'
     
     # Setup first frame for re-use ( inc. basemap ) and get key variables 
-    cmap, specplt, clevs, cnorm, minc, maxc, m = setup_plot2animate( arr,\
-            fig=fig, ax=ax, lat=lat, lon=lon, units=units, res=res, \
-            fixcb=fixcb, debug=debug )
+    cmap, specplt, lvls, cnorm, m, fixcb, fixcb_buffered = setup_plot2animate(\
+             arr,fig=fig, ax=ax, lat=lat, lon=lon, \
+            units=units, res=res, fixcb=fixcb, debug=debug )
 
     # setup figure ascetics
-    setup_figure_ascetics( dates, cmap=cmap, cnorm=cnorm, maxc=maxc, \
-        minc=minc, units=units, fig=fig, title=title, extend=extend, \
-        arr=arr, fixcb=fixcb, debug=debug)
+    setup_figure_ascetics( dates, cmap=cmap, cnorm=cnorm, \
+        units=units, fig=fig, title=title, extend=extend, \
+        arr=arr, fixcb=fixcb, fixcb_buffered=fixcb_buffered, debug=debug)
     
     # animate the array and save as 
-    animate_array( arr, dates, specplt, clevs=clevs, cnorm=cnorm, \
+    animate_array( arr, dates, specplt, lvls=lvls, cnorm=cnorm, \
         cmap=cmap, debug=debug, fig=fig, m=m, lon=lon, lat=lat, \
         spec=spec, fname=fname )
 
-def get_data_dates( spec='O3', dates_variable='time', \
-            fill_invalid_with_mean=True, limit_by_dates=False, 
-            sdate=datetime.datetime(2005, 01, 01), 
-            edate=datetime.datetime(2005, 01, 07), debug=False ):
+def extract_data_dates( spec='O3', file=None, dates_variable='time', \
+            fill_invalid_with_mean=True, limit_by_dates=False, \
+            sdate=datetime.datetime(2005, 01, 01), ver = '1.7', \
+            edate=datetime.datetime(2005, 01, 07), debug=False  ):
     """ Extracts dates and data from a given location """
 
-    import numpy as np
     from pandas import DataFrame
+    import numpy as np
     from netCDF4 import Dataset
     import datetime
-    from AC_tools.funcs_vars import get_dir
 
-    # Set file to use
-    wd = get_dir('npwd')
-#    wd = '/work/home/ts551/temp/'
-#    f= 'pf_iGEOSChem_1.7_v10_G5_EU_run.0.25x0.3125.2012.1week.sucess_3D.nc'
-    f='pf_iGEOSChem_1.7_v10_G5_EU_run.0.25x0.3125.2015.II.spun.up_3D.nc'
-#    f= 'pf_iGEOSChem_1.7_v10_G5_EU_run_3D.nc'
-    print wd + f
-    # Extract data
-    with Dataset( wd+f , 'r' ) as rootgrp:
+#    # <= Kludge: convert to tracer name used in NetCDF for extraction
+    from AC_tools.funcs_vars import what_species_am_i
+    pspec = what_species_am_i( input=spec, ver=ver, invert=True, debug=debug  )    
+
+    with Dataset( file, 'r' ) as rootgrp:
 
         if debug:
             print [i for i in rootgrp.variables ]
 
-        # return data as an array
-#        arr = np.ma.array( rootgrp.variables[ spec ]   )
-        arr = np.array( rootgrp.variables[ spec ]   )
-        print rootgrp.variables[ spec ] 
-        print np.array( rootgrp.variables[ spec ]  )
+        # Return data as an array
+#        arr = np.ma.array( rootgrp.variables[ pspec ]   )
+        arr = np.array( rootgrp.variables[ pspec ]   )
+        print rootgrp.variables[ pspec ] 
+        print np.array( rootgrp.variables[ pspec ]  )
 
         # get dates
         dates = np.ma.array( rootgrp.variables[ dates_variable ]   )
@@ -82,7 +81,7 @@ def get_data_dates( spec='O3', dates_variable='time', \
         print [ ( type( i ), i.shape ) for i in arr, dates ]
         print [ (i.min(), i.max(), i.mean()) for i in arr, dates ]
 
-    # mask with invalid values ( fill values in array) 
+    # Mask with invalid values ( fill values in array) 
     arr = np.ma.masked_invalid( arr  )
     if debug:
         print [ (i.min(), i.max(), i.mean()) for i in [arr] ]
@@ -94,14 +93,12 @@ def get_data_dates( spec='O3', dates_variable='time', \
     if debug:
         print [ i.shape for i in arr, dates ]
     
-    print edate, sdate, dates[0]
-    print [ type(i) for i in edate, sdate, dates[0] ]
+        print edate, sdate, dates[0]
+        print [ type(i) for i in edate, sdate, dates[0] ]
     
-    print dates > sdate 
-    print dates < edate 
-    
-#    print dates[ np.where( (dates >= sdate)  ) ] 
-    
+        print dates > sdate 
+        print dates < edate 
+ 
     # Limit to given dates ( e.g. 1st month 2005)
     if limit_by_dates:
         dates = dates[ np.where( dates >= sdate ) ] 
@@ -114,7 +111,57 @@ def get_data_dates( spec='O3', dates_variable='time', \
         arr = arr[ np.where( dates < edate ), ... ][0,...] 
         print [i.shape for i in arr, dates ]
         
-        
+    return arr, dates
+
+def get_data_dates( spec='O3', dates_variable='time', \
+            fill_invalid_with_mean=True, limit_by_dates=False, \
+            sdate=datetime.datetime(2005, 01, 01), pcent=False, \
+            edate=datetime.datetime(2005, 01, 07), debug=False ):
+    """ Set dirs/files to use.
+        Extracts dates and data from a given location
+        NOTE:
+           (A) If more than 1 file given then a difference, in actual or 
+           percent (if pcent==True) terms is returned 
+          
+    """
+    import numpy as np
+    from AC_tools.funcs_vars import get_dir
+
+    # Set Directory ( comment out if using different earth0 user )
+    wd = get_dir('npwd')
+
+    # Set files to use
+    files = [    
+'pf_iGEOSChem_1.7_v10_G5_EU.EOH_run.x50SUMMER_ClearFlo.EMEPx4.RETROx4.no.EOH.emiss.OH_output_3D.nc',
+'pf_iGEOSChem_1.7_v10_G5_EU.EOH_run.x50SUMMER_ClearFlo.EMEPx4.RETROx4.OH_output.ALD2_3D.nc'
+    ]    
+    files =  [ wd+i for i in files ]
+    print files
+
+    # Extract data, by looping file list 
+    data_l, dates_l = [] , []
+    for file in files:
+        data_, dates_ = extract_data_dates( spec=spec, file=file, \
+            dates_variable=dates_variable, limit_by_dates=limit_by_dates,\
+            fill_invalid_with_mean=fill_invalid_with_mean, debug=debug ) 
+        dates_l += [ dates_ ]
+        data_l += [ data_ ]        
+
+    # If more than one data set - get difference
+    if len( files ) > 1:
+        # Get change in concentration
+        # get % change in conc
+        if pcent:
+            arr = ( data_l[1]-data_l[0] ) / data_l[0] *100
+        else:
+            arr = data_l[1]-data_l[0] 
+        dates = dates_l[0] # assume date arrays are the same 
+    else:
+        dates, arr = dates_l[0], data_l[0]
+
+    # Kludge for testing ( just use 1st few times )
+    dates, arr = dates[:10], arr[:10,...]
+
     # The corner point is a NaN for 0.25 output.  <= improve this. 
     # Set this ( not visible on the plot window ) to mean to allow for save
     if fill_invalid_with_mean:
@@ -124,14 +171,15 @@ def get_data_dates( spec='O3', dates_variable='time', \
     return arr, dates
 
 def get_run_info( spec='O3', res='0.25x0.3125', region='EU', fname='', \
-            scale=1, pcent=False ):
+            scale=1, pcent=False, ClearFlo_unit=False ):
     """ get descriptive variables for run period ( e.g. res ) """
     from AC_tools.funcs4core import get_latlonalt4res
     from AC_tools.funcs_vars import tra_unit, latex_spec_name
     
     # Set variables (e.g. res) or automation of variable setting here
 #    res = get_run_descriptors()
-#    res='0.5x0.666'
+    # Kludge set res manually
+    res='0.5x0.666'
     
     # Get lat and lon of GC grid for given resolution
     lon, lat, NIU = get_latlonalt4res( res=res )
@@ -140,7 +188,7 @@ def get_run_info( spec='O3', res='0.25x0.3125', region='EU', fname='', \
     if pcent:
         units ='%'        
     else:
-        units, scale = tra_unit( spec, scale=True)
+        units, scale = tra_unit( spec, ClearFlo_unit=ClearFlo_unit, scale=True)
 
     # Set filename from run detail
     fname += '{}_{}_{}_{}_{}.mp4'.format( region, res, 
@@ -150,7 +198,6 @@ def get_run_info( spec='O3', res='0.25x0.3125', region='EU', fname='', \
     title = 'Surface {} / {}'.format( latex_spec_name( spec ), units )
     
     return lat, lon, units, fname, res, title, scale
-
 
 def setup_figure_and_axis( ):
     """ Initialise figure and axis """
@@ -163,27 +210,27 @@ def setup_figure_and_axis( ):
     return fig, ax
 
 def setup_figure_ascetics(  dates, f_size=10, title=None, cmap=None, \
-            minc=None, units=None, cnorm=None, fig=None, maxc=None,\
-            format='%.0f', extend='neither', arr=None, fixcb=None, debug=False):
+            units=None, cnorm=None, fig=None, format='%.0f', \
+            extend='neither', arr=None, fixcb=None, \
+            fixcb_buffered=None, debug=False):
     """ Add colorbar, logos and titles to figure """
 
     # if on Univeristy of York/NCAS servers, add logos
     import platform
-    if platform.platform() == 'Linux-3.0.101-0.47.52-default-x86_64-with-SuSE-11-x86_64':
-        from funcs4plotting_special import add_logos_NCAS_york_bottom, mk_cb
+#    earth0 = 'Linux-3.0.101-0.47.52-default-x86_64-with-SuSE-11-x86_64'
+    earth0 = 'Linux-3.0.101-0.47.71-default-x86_64-with-SuSE-11-x86_64'
+    if platform.platform() == earth0:
+        from funcs4plotting_special import add_logos_NCAS_york_bottom
 
         # add title and logos for NCAS/NERC
         fig = add_logos_NCAS_york_bottom( fig)
         fig.suptitle( title, fontsize=f_size*2, x=.55 , y=.95  )
 
-    # Set min and max of colorbar if not given
-    if isinstance( fixcb, type(None) ):
-        fixcb = ( minc, maxc )
-
     # Add colorbar  
+    from funcs4plotting_special import mk_cb
     mk_cb(fig, units=units, left=0.925, bottom=0.2, width=0.015, height=0.6, \
-        cmap=cmap, vmin=fixcb[0], vmax=fixcb[1], f_size=f_size, format=format, \
-        norm=cnorm, extend=extend,  ) 
+        cmap=cmap, vmin=fixcb_buffered[0], vmax=fixcb_buffered[1],  \
+        f_size=f_size, format=format, norm=cnorm, extend=extend,  ) 
 
     # Add text showing model time on figure 
     plt.figtext(x=0.5,y=0.875, s=dates[0], fontsize=f_size)
@@ -192,19 +239,16 @@ def setup_plot2animate( arr, fig=None, ax=None, lat=None, lon=None, \
         units=None, contour=False, res=None, f_size=20, min_change=0.5,  \
         period=1, adjust_window=3, alpha=0.85, npoints=50, \
         everyother=1, interval=1, resolution='f', drawcountries=True, \
-        cnorm = None, clevs=None, fixcb=None, debug=False ):
+        cnorm = None, lvls=None, fixcb=None, positive=None, \
+        nticks=10, sigfig_rounding_on_cb=2, debug=False ):
 
-    from funcs4plotting_special import get_basemap, get_colormap
+    from funcs4plotting_special import get_basemap, get_colormap, \
+            get_human_readable_gradations
 
     # --- settings
     plt.ioff() # turn off interactive plotting
     global specplt
 
-    # Get colormap for fixed length or min/max or array
-    if isinstance( fixcb, type(None) ):
-        fixcb = arr.copy() 
-    cmap = get_colormap( fixcb )
-    
     print 3, 'detail on output: ', [ [ np.ma.min(i), np.ma.max(i), \
         np.ma.mean(i), type(i),i.shape ] for i in [arr] ]
 
@@ -224,21 +268,29 @@ def setup_plot2animate( arr, fig=None, ax=None, lat=None, lon=None, \
     # Make sure array is a masked numpy array
     arr  = np.ma.array( arr )
 
+    # Get min and max value in whole array
+    fixcb= [arr.min(),  arr.max() ]
+
+    # Set readable levels for cb, then use these to dictate cmap
+    lvls = get_human_readable_gradations( vmax=fixcb[1],  \
+                    vmin=fixcb[0], nticks=nticks, \
+                    sigfig_rounding_on_cb=sigfig_rounding_on_cb  )
+        
     # Setup colour or pcolor plot
     if contour:
-        minc=arr.min()
-        maxc=arr.max()
-
-        clevs = np.linspace(minc, maxc+min_change, npoints )
-        cnorm = mpl.colors.Normalize(vmin=minc, vmax=maxc)
-        specplt = m.contourf(x,y,arr[0,:,:],clevs,cmap=cmap, 
-            latlon=True, norm=cnorm,antialiased=True)
+        # updated to use readable levels, this needs testing 
+#        lvls = np.linspace(minc, maxc+min_change, npoints )
+        cnorm = mpl.colors.Normalize(vmin=fixcb[0], vmax=fixcb[1])
+        specplt = m.contourf(x,y,arr[0,:,:], lvls, cmap=cmap, 
+            latlon=True, norm=cnorm, antialiased=True)
 
         fname = 'contour_'+fname
 
     else:
-        minc=arr.min()
-        maxc=arr.max()
+        # Setup Colormap
+        cmap, fixcb_buffered = get_colormap( np.array( fixcb ), \
+            nticks=nticks, fixcb=fixcb, buffer_cmap_upper=True )
+
         specplt = m.pcolor( lon, lat, arr[0,:,:], cmap=cmap)
         #, antialiased=True )
 
@@ -247,11 +299,11 @@ def setup_plot2animate( arr, fig=None, ax=None, lat=None, lon=None, \
         print arr.shape, type( arr) 
         print arr.shape, type( arr) 
 
-    return cmap, specplt, clevs, cnorm, minc, maxc, m 
+    return cmap, specplt, lvls, cnorm, m, fixcb, fixcb_buffered
 
 def animate_array( arr, dates, specplt, spec='O3', min_change=0.5, \
         period=1, adjust_window=3, alpha=0.85, npoints=50, wd='./', \
-        clevs=None, cnorm=None, cmap=None, contour=False, \
+        lvls=None, cnorm=None, cmap=None, contour=False, \
         fig=None, m=None, lon=None, lat=None, fname=None, debug=False ):
     """ Animates array with specplt as first frame"""
 
@@ -264,22 +316,22 @@ def animate_array( arr, dates, specplt, spec='O3', min_change=0.5, \
         print i, 'for {} @ model time of {} @  real time of {}'.format( spec, \
              dates[i], time.strftime('%Y %m %d %H:%M:%S') )
         if contour:
-            # remove previous plot data, but retain basemap....
+            # Remove previous plot data, but retain basemap....
             for c in specplt.collections:
                 c.remove()
             # Fill basemap 
-            specplt = m.contourf(x,y,arr[i,:,:], clevs, cmap=cmap, 
+            specplt = m.contourf(x,y,arr[i,:,:], lvls, cmap=cmap, 
                 latlon=True, norm=cnorm, alpha=alpha, antialiased=True)
         else:
-            # remove previous plot data, but retain basemap....
+            # Remove previous plot data, but retain basemap....
             specplt.remove()
             # Fill basemap/ plot
             specplt = m.pcolor( lon, lat, arr[i,:,:], cmap=cmap)
 
-        # clean memory
+        # Clean memory
         gc.collect()
 
-        # update date
+        # Update date
         fig.texts[-1].set_text( dates[i] )
         return specplt
 
@@ -292,6 +344,12 @@ def animate_array( arr, dates, specplt, spec='O3', min_change=0.5, \
             'libx264','-pix_fmt', 'yuv420p'])
     print 'Video saved & Closed as/at: ', fname
 
+    # Clean memory
+#    gc.collect()    
+#    plt.clf()
+#    del specplt
+    
 if __name__ == "__main__":
     for spec in specs:
-        main( spec=spec, debug=debug )
+        main( spec=spec, pcent=pcent, ClearFlo_unit=ClearFlo_unit, \
+                    debug=debug )
