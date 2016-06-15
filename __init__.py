0c78f220ad21c323abf2eac70f957d532aa8d2b5
Ben Newsome <bn506@york.ac.uk>
2016-06-15 15:31:21 +0100

    Added a Description for MChem_tools

---
+++ __init__.py
@@ -0,0 +1,17 @@
+"""
+Some Documentation about MChem_tools goes here. All modules should have this in the
+__init__ folder at the top, in tripple double quotes - pep
+
+MChem_tools is a bunch of scripts created by Tomas and hopefully maintained by the Evans
+Group.
+
+To access the help, from python or ipython, type help(MChem_tools) to get general help
+
+To get more detailed help from a module for example, type help(MChem_tools.AC_tools)
+
+If you find missing documentation in any of this, please request a git push to github.
+
+Initial documentation written by Ben - expect spelling mistakes.
+
+"""
+
